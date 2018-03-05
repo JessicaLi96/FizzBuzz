7585a80a75a53e52c1433f6c7eff2d3572c5e673
JessicaLi96 <36928340+JessicaLi96@users.noreply.github.com>
2018-03-05 11:42:32 -0500

    Add files via upload
    fizzbuzz

---
+++ FizzBuzz.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                //Console.WriteLine("initial number =  " + i);
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0 && i % 3 != 0)
+                {
+                    Console.WriteLine("Buzz");
+
+                }
+                else if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else { Console.WriteLine(i); }
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
